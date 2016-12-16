f1509b8b0562b2606e2a9e952e5ccd5411df3c8c
gyver98 <matthewchoi7@gmail.com>
2016-12-16 17:26:10 +1100

    Step 4: ADD UI for Header

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,16 +1,30 @@
 var React = require('react');
+var GetCityContainer = require('../containers/GetCityContainer');
+
 var styles = {
   container: {
     width: '100%',
     height: '92%'
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'rgba(252, 90, 44, 0.89)',
+    color: '#fff',
+    padding: 5
   }
 }
+
 
 var Main = React.createClass({
   render: function () {
     return (
       <div style={styles.container}>
-        <h1>Main.js Header</h1>
+        <div style={styles.header}>
+            <h2 style={{margin: 0}}>Weather</h2>
+            <GetCityContainer direction='row'/>
+        </div>
         {this.props.children}
       </div>
     )
