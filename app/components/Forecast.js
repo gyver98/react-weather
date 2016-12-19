4da5ca2f3131a6c5982f6b2322604dc7caebe023
gyver98 <matthewchoi7@gmail.com>
2016-12-19 14:01:23 +1100

    Step 6: New /forecast route

---
+++ app/components/Forecast.js
@@ -0,0 +1,30 @@
+var React = require('react');
+var ForecastContainer = require('../containers/ForecastContainer');
+
+// var styles = {
+//   container: {
+//     backgroundSize: 'cover',
+//     backgroundImage: "url('app/images/pattern.svg')",
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     height: '100%',
+//     width: '100%'
+//   },
+//   header: {
+//     fontSize: 45,
+//     color: '#fff',
+//     fontWeight: 100,
+//   },
+// }
+
+function Forecast (props) {
+    return (
+        <div>
+            Forecast dispalyed
+        </div>
+    )
+}
+
+module.exports = Forecast;
